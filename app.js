48680903ce069b453f307065a9273ca37289273a
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-07-03 16:04:11 +0800

    <feat>[AC-S2-3-W1-Restaurant-List]: Remove redundant code

--- app.js
+++ app.js
@@ -23,16 +23,6 @@ const restaurantList = require('./restaurant.json')
 // Handle request and response here
 // to Get restaurant list by restaurant.json
 app.get('/', (req, res) => {
-  // create a variable to store restaurantOne
-  const restaurantOne = {
-    id: 1,
-    name: 'Sababa 沙巴巴中東美食',
-    category: '中東料理',
-    rating: 4.1,
-    image:
-      'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5635/01.jpg',
-  }
-
   // past the movie data into 'index' partial template
   res.render('index', { restaurants: restaurantList.results })
 })
@@ -42,7 +32,7 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(
     (restaurant) => restaurant.id.toString() === req.params.restaurant_id
   )
-  res.render('show', { restaurant: restaurant })
+  res.render('show', { restaurant })
 })
 
 // to Search restaurant by name or category
